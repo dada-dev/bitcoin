5bb8931f17b1ca661186be816bedbbdf50c565a3
c3c1l1a <cesswangumukima@gmail.com>
2025-02-20 02:46:54 +0800

    Test PR on the bitcoin codebase

--- test/functional/feature_addrman.py
+++ test/functional/feature_addrman.py
@@ -100,12 +100,12 @@ class AddrmanTest(BitcoinTestFramework):
         self.log.info("Check that corrupt addrman cannot be read (magic)")
         self.stop_node(0)
         write_addrman(peers_dat, net_magic="signet")
-        self.nodes[0].assert_start_raises_init_error(
-            expected_msg=init_error("Invalid network magic number"),
-            match=ErrorMatch.FULL_REGEX,
-        )
+        # self.nodes[0].assert_start_raises_init_error(
+        #     expected_msg=init_error("Invalid network magic number"),
+        #     match=ErrorMatch.FULL_REGEX,
+        # )
 
-        self.log.info("Check that corrupt addrman cannot be read (checksum)")
+        # self.log.info("Check that corrupt addrman cannot be read (checksum)")
         self.stop_node(0)
         write_addrman(peers_dat, mock_checksum=b"ab" * 32)
         self.nodes[0].assert_start_raises_init_error(
